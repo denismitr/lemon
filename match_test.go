d0c1ec8dea0e8fbd9a0aaf1995c3fa4c5bf4bf49
Denis Mitrofanov <denis.mitr@gmail.com>
2021-09-18 18:12:33 +0300

    fixes untag tests

--- match_test.go
+++ match_test.go
@@ -3,6 +3,7 @@ package lemon_test
 import (
 	"context"
 	"github.com/denismitr/lemon"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/suite"
 	"os"
 	"sync"
@@ -80,7 +81,7 @@ func (uts *untagTestSuite) TestUntagSingleProduct() {
 	uts.Assert().Nil(tagsAfterUntag["type"])
 }
 
-func (uts *untagTestSuite) TestUntagMultipleProduct() {
+func (uts *untagTestSuite) Test_Attempt_Untag_NonExistingKey() {
 	db, closer, err := lemon.Open(uts.fixture)
 	uts.Require().NoError(err)
 
@@ -90,13 +91,30 @@ func (uts *untagTestSuite) TestUntagMultipleProduct() {
 		}
 	}()
 
-	productBeforeUntag, err := db.Get("product:34")
-	uts.Require().NoError(err)
-	uts.Require().NotNil(productBeforeUntag)
+	utErr := db.Untag(context.Background(), "non:existing:key", "foo", "bar", "baz")
+	uts.Require().Error(utErr)
+	uts.Require().True(errors.Is(utErr, lemon.ErrKeyDoesNotExist))
+}
 
-	tagsBeforeUntag := productBeforeUntag.Tags()
-	uts.Assert().Equal(23.45, tagsBeforeUntag["price"])
-	uts.Assert().Equal("tv", tagsBeforeUntag["type"])
+func (uts *untagTestSuite) Test_Untag_MultipleProduct() {
+	db, closer, err := lemon.Open(uts.fixture)
+	uts.Require().NoError(err)
+
+	defer func() {
+		if err := closer(); err != nil {
+			uts.T().Errorf("DB CLOSE ERROR: %v", err)
+		}
+	}()
+
+	p11BeforeUntag, err := db.Get("product:11")
+	uts.Require().NoError(err)
+	uts.Require().NotNil(p11BeforeUntag)
+	uts.Require().Equal(lemon.M{"inStock": 8, "price": 9.45, "type": "tv"}, p11BeforeUntag.Tags())
+
+	p10BeforeUntag, err := db.Get("product:10")
+	uts.Require().NoError(err)
+	uts.Require().NotNil(p10BeforeUntag)
+	uts.Require().Equal(lemon.M{"inStock": 2, "price": 10.45, "type": "tv"}, p10BeforeUntag.Tags())
 
 	var p11TagsBeforeCommit lemon.M
 	var p10TagsBeforeCommit lemon.M
@@ -122,17 +140,17 @@ func (uts *untagTestSuite) TestUntagMultipleProduct() {
 
 	// expect tags before commit and after commit be equal
 	uts.Assert().Equal(lemon.M{}, p11TagsBeforeCommit)
-	uts.Assert().Equal(lemon.M{"inStock":2, "type":"tv"}, p10TagsBeforeCommit)
+	uts.Assert().Equal(lemon.M{"inStock": 2, "type": "tv"}, p10TagsBeforeCommit)
 
-	p10, err := db.Get("product:10")
+	p10AfterUntag, err := db.Get("product:10")
 	uts.Require().NoError(err)
-	uts.Require().NotNil(p10)
-	uts.Assert().Equal(lemon.M{"inStock":2, "type":"tv"}, p10.Tags())
+	uts.Require().NotNil(p10AfterUntag)
+	uts.Assert().Equal(lemon.M{"inStock": 2, "type": "tv"}, p10AfterUntag.Tags())
 
-	p11, err := db.Get("product:11")
+	p11AfterUntag, err := db.Get("product:11")
 	uts.Require().NoError(err)
-	uts.Require().NotNil(p11)
-	uts.Assert().Equal(lemon.M{}, p11.Tags())
+	uts.Require().NotNil(p11AfterUntag)
+	uts.Assert().Equal(lemon.M{}, p11AfterUntag.Tags())
 }
 
 type matchTestSuite struct {
@@ -193,7 +211,7 @@ func (mts *matchTestSuite) TestMatchSingleUserByPatternAndTag() {
 	mts.Require().Equal("user:12:animals", docs[0].Key())
 	mts.Require().Equal(`[123, 987, 6789]`, docs[0].RawString())
 	mts.Require().Equal(123, docs[0].JSON().IntOrDefault(`0`, 0))
-	mts.Require().Equal(lemon.M{"content":"list"}, docs[0].Tags())
+	mts.Require().Equal(lemon.M{"content": "list"}, docs[0].Tags())
 	mts.Require().Equal("list", docs[0].Tags().String("content"))
 }
 
@@ -221,12 +239,12 @@ func (mts *matchTestSuite) TestMatchMultipleUsersByPatternAndGtIntTag() {
 
 	mts.Require().Equal("user:124", docs[0].Key())
 	mts.Require().Equal(`{"1900-10-20":null,"bar":{"a":666,"b":"baz223"},"foo":124}`, docs[0].RawString())
-	mts.Require().Equal(lemon.M{"age":58, "auth":"basic", "content":"other", "valid":false}, docs[0].Tags())
+	mts.Require().Equal(lemon.M{"age": 58, "auth": "basic", "content": "other", "valid": false}, docs[0].Tags())
 	mts.Require().Equal("other", docs[0].Tags().String("content"))
 
 	mts.Require().Equal("user:125", docs[1].Key())
 	mts.Require().Equal(`{"1900-10-20":0,"bar":{"a":667,"b":"baz123223"},"foo":125}`, docs[1].RawString())
-	mts.Require().Equal(lemon.M{"age":59, "auth":"basic", "content":"doc", "valid":true}, docs[1].Tags())
+	mts.Require().Equal(lemon.M{"age": 59, "auth": "basic", "content": "doc", "valid": true}, docs[1].Tags())
 	mts.Require().Equal("doc", docs[1].Tags().String("content"))
 	mts.Require().Equal(59, docs[1].Tags().Int("age"))
 	mts.Require().Equal(true, docs[1].Tags().Bool("valid"))
@@ -258,25 +276,25 @@ func (mts *matchTestSuite) TestMatchMultipleUsersByPatternAndTagWithDescSorting(
 
 	mts.Require().Equal("user:125", docs[0].Key())
 	mts.Require().Equal(`{"1900-10-20":0,"bar":{"a":667,"b":"baz123223"},"foo":125}`, docs[0].RawString())
-	mts.Require().Equal(lemon.M{"age":59, "auth":"basic", "content":"doc", "valid":true}, docs[0].Tags())
+	mts.Require().Equal(lemon.M{"age": 59, "auth": "basic", "content": "doc", "valid": true}, docs[0].Tags())
 	mts.Require().Equal("doc", docs[0].Tags().String("content"))
 	mts.Require().Equal(true, docs[0].Tags().Bool("valid"))
 
 	mts.Require().Equal("user:123", docs[1].Key())
 	mts.Require().Equal(`{"1900-10-20":678.345,"bar":{"a":987,"b":"baz"},"id":123}`, docs[1].RawString())
-	mts.Require().Equal(lemon.M{"auth":"token", "content":"doc", "valid":true}, docs[1].Tags())
+	mts.Require().Equal(lemon.M{"auth": "token", "content": "doc", "valid": true}, docs[1].Tags())
 	mts.Require().Equal("doc", docs[1].Tags().String("content"))
 	mts.Require().Equal(true, docs[1].Tags().Bool("valid"))
 
 	mts.Require().Equal("user:12", docs[2].Key())
 	mts.Require().Equal(`{"1900-10-20":10.345,"bar":{"a":1234567,"b":"baz22"},"id":12}`, docs[2].RawString())
-	mts.Assert().Equal(lemon.M{"auth":"token", "content":"doc", "valid":true}, docs[2].Tags())
+	mts.Assert().Equal(lemon.M{"auth": "token", "content": "doc", "valid": true}, docs[2].Tags())
 	mts.Require().Equal("doc", docs[2].Tags().String("content"))
 	mts.Require().Equal(true, docs[2].Tags().Bool("valid"))
 
 	mts.Require().Equal("user:9", docs[3].Key())
 	mts.Require().Equal(`{"1900-11-20":0.04,"bar":{"a":555,"b":"foo1234"},"id":9}`, docs[3].RawString())
-	mts.Require().Equal(lemon.M{"age":55, "auth":"basic", "content":"doc", "foo":"bar", "valid":true}, docs[3].Tags())
+	mts.Require().Equal(lemon.M{"age": 55, "auth": "basic", "content": "doc", "foo": "bar", "valid": true}, docs[3].Tags())
 	mts.Require().Equal("doc", docs[3].Tags().String("content"))
 	mts.Require().Equal("bar", docs[3].Tags().String("foo"))
 	mts.Require().Equal(55, docs[3].Tags().Int("age"))
@@ -309,19 +327,19 @@ func (mts *matchTestSuite) TestMatchMultipleUsersByPatternAndTagWithAscSorting()
 
 	mts.Require().Equal("user:125", docs[2].Key())
 	mts.Require().Equal(`{"1900-10-20":0,"bar":{"a":667,"b":"baz123223"},"foo":125}`, docs[2].RawString())
-	mts.Require().Equal(lemon.M{"age":59, "auth":"basic", "content":"doc", "valid":true}, docs[2].Tags())
+	mts.Require().Equal(lemon.M{"age": 59, "auth": "basic", "content": "doc", "valid": true}, docs[2].Tags())
 	mts.Require().Equal("doc", docs[2].Tags().String("content"))
 	mts.Require().Equal(true, docs[2].Tags().Bool("valid"))
 
 	mts.Require().Equal("user:124", docs[1].Key())
 	mts.Require().Equal(`{"1900-10-20":null,"bar":{"a":666,"b":"baz223"},"foo":124}`, docs[1].RawString())
-	mts.Assert().Equal(lemon.M{"age":58, "auth":"basic", "content":"other", "valid":false}, docs[1].Tags())
+	mts.Assert().Equal(lemon.M{"age": 58, "auth": "basic", "content": "other", "valid": false}, docs[1].Tags())
 	mts.Require().Equal("other", docs[1].Tags().String("content"))
 	mts.Require().Equal(false, docs[1].Tags().Bool("valid"))
 
 	mts.Require().Equal("user:9", docs[0].Key())
 	mts.Require().Equal(`{"1900-11-20":0.04,"bar":{"a":555,"b":"foo1234"},"id":9}`, docs[0].RawString())
-	mts.Assert().Equal(lemon.M{"age":55, "auth":"basic", "content":"doc", "foo":"bar", "valid":true}, docs[0].Tags())
+	mts.Assert().Equal(lemon.M{"age": 55, "auth": "basic", "content": "doc", "foo": "bar", "valid": true}, docs[0].Tags())
 	mts.Require().Equal("doc", docs[0].Tags().String("content"))
 	mts.Require().Equal("bar", docs[0].Tags().String("foo"))
 	mts.Require().Equal(55, docs[0].Tags().Int("age"))
@@ -353,7 +371,7 @@ func (mts *matchTestSuite) TestMatchSingleUsersByPreciseAge() {
 
 	mts.Require().Equal("user:9", docs[0].Key())
 	mts.Require().Equal(`{"1900-11-20":0.04,"bar":{"a":555,"b":"foo1234"},"id":9}`, docs[0].RawString())
-	mts.Require().Equal(lemon.M{"age":55, "auth":"basic", "content":"doc", "foo":"bar", "valid":true}, docs[0].Tags())
+	mts.Require().Equal(lemon.M{"age": 55, "auth": "basic", "content": "doc", "foo": "bar", "valid": true}, docs[0].Tags())
 	mts.Require().Equal("doc", docs[0].Tags().String("content"))
 	mts.Require().Equal("bar", docs[0].Tags().String("foo"))
 	mts.Require().Equal(55, docs[0].Tags().Int("age"))
@@ -422,35 +440,35 @@ func seedGranularUsers(t *testing.T, db *lemon.DB, wg *sync.WaitGroup) {
 
 	err := db.Update(context.Background(), func(tx *lemon.Tx) error {
 		if err := tx.Insert("user:12", lemon.M{
-				"id": 12,
-				"bar": lemon.M{
-					"a": 1234567,
-					"b": "baz22",
-				},
-				"1900-10-20": 10.345,
-			}, lemon.WithTags().Map(lemon.M{
-				"content": "doc",
-				"auth": "token",
-				"valid":   true,
-			}),
+			"id": 12,
+			"bar": lemon.M{
+				"a": 1234567,
+				"b": "baz22",
+			},
+			"1900-10-20": 10.345,
+		}, lemon.WithTags().Map(lemon.M{
+			"content": "doc",
+			"auth":    "token",
+			"valid":   true,
+		}),
 		); err != nil {
 			return err
 		}
 
 		if err := tx.Insert("user:9", lemon.M{
-				"id": 9,
-				"bar": lemon.M{
-					"a": 555,
-					"b": "foo1234",
-				},
-				"1900-11-20": 0.04,
-			}, lemon.WithTags().Map(lemon.M{
-				"content": "doc",
-				"foo":     "bar",
-				"auth": "basic",
-				"age": 55,
-				"valid":   true,
-			}),
+			"id": 9,
+			"bar": lemon.M{
+				"a": 555,
+				"b": "foo1234",
+			},
+			"1900-11-20": 0.04,
+		}, lemon.WithTags().Map(lemon.M{
+			"content": "doc",
+			"foo":     "bar",
+			"auth":    "basic",
+			"age":     55,
+			"valid":   true,
+		}),
 		); err != nil {
 			return err
 		}
@@ -482,27 +500,27 @@ func seedGranularUsers(t *testing.T, db *lemon.DB, wg *sync.WaitGroup) {
 			},
 			lemon.WithTags().Map(lemon.M{
 				"content": "other",
-				"auth": "basic",
-				"valid": false,
-				"age": 58,
+				"auth":    "basic",
+				"valid":   false,
+				"age":     58,
 			}),
 		); err != nil {
 			return err
 		}
 
 		if err := tx.Insert("user:125", lemon.M{
-				"foo": 125,
-				"bar": lemon.M{
-					"a": 667,
-					"b": "baz123223",
-				},
-				"1900-10-20": 0.0,
-			}, lemon.WithTags().Map(lemon.M{
-				"content": "doc",
-				"auth": "basic",
-				"valid": true,
-				"age": 59,
-			}),
+			"foo": 125,
+			"bar": lemon.M{
+				"a": 667,
+				"b": "baz123223",
+			},
+			"1900-10-20": 0.0,
+		}, lemon.WithTags().Map(lemon.M{
+			"content": "doc",
+			"auth":    "basic",
+			"valid":   true,
+			"age":     59,
+		}),
 		); err != nil {
 			return err
 		}
@@ -594,8 +612,8 @@ func seedGranularTvProducts(t *testing.T, db *lemon.DB, wg *sync.WaitGroup) {
 		if err := tx.Insert(
 			"product:10",
 			lemon.M{
-				"vendor": "LG",
-				"model": "AFK1",
+				"vendor":  "LG",
+				"model":   "AFK1",
 				"version": 4.2,
 			},
 			lemon.WithTags().
@@ -609,8 +627,8 @@ func seedGranularTvProducts(t *testing.T, db *lemon.DB, wg *sync.WaitGroup) {
 		if err := tx.Insert(
 			"product:7",
 			lemon.M{
-				"vendor": "LG",
-				"model": "AFK2",
+				"vendor":  "LG",
+				"model":   "AFK2",
 				"version": 4.3,
 			},
 			lemon.WithTags().
@@ -624,8 +642,8 @@ func seedGranularTvProducts(t *testing.T, db *lemon.DB, wg *sync.WaitGroup) {
 		if err := tx.Insert(
 			"product:11",
 			lemon.M{
-				"vendor": "Sony",
-				"model": "Bravia-22",
+				"vendor":  "Sony",
+				"model":   "Bravia-22",
 				"version": 4.3,
 			},
 			lemon.WithTags().
@@ -653,8 +671,8 @@ func seedWebPages(t *testing.T, db *lemon.DB, wg *sync.WaitGroup) {
 			loadFixtureContents(t, "./__fixtures__/web4.html"),
 			lemon.WithTags().Map(lemon.M{
 				"content": "html",
-				"auth": "none",
-				"active":   true,
+				"auth":    "none",
+				"active":  true,
 			}),
 		); err != nil {
 			return err
@@ -665,8 +683,8 @@ func seedWebPages(t *testing.T, db *lemon.DB, wg *sync.WaitGroup) {
 			loadFixtureContents(t, "./__fixtures__/web3.html"),
 			lemon.WithTags().Map(lemon.M{
 				"content": "html",
-				"auth": "none",
-				"active":   true,
+				"auth":    "none",
+				"active":  true,
 			}),
 		); err != nil {
 			return err
