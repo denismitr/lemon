66f5ddc9a3eba7318bf8364ba152bac9a8d7bf3a
Denis Mitrofnov <dmitrofanov@more.tv>
2021-12-28 18:47:18 +0300

    adds lru sharded cache

---
+++ internal/lru/cache.go
@@ -0,0 +1,57 @@
+package lru
+
+import (
+	"encoding/binary"
+	"github.com/cespare/xxhash/v2"
+	"github.com/pkg/errors"
+)
+
+var ErrIllegalCapacity = errors.New("illegal lru cache capacity")
+var ErrInvalidSharding = errors.New("invalid sharding")
+
+type Cache struct {
+	maxBytes uint64
+	capacity uint64
+	shards []*lruShard
+}
+
+func NewCache(shards int, maxBytes uint64) (*Cache, error) {
+	if maxBytes <= 2 {
+		return nil, ErrIllegalCapacity
+	}
+
+	if shards <= 2 {
+		return nil, ErrInvalidSharding
+	}
+
+	c := Cache{
+		maxBytes: maxBytes,
+		capacity: uint64(shards),
+		shards: make([]*lruShard, shards),
+	}
+
+	shardMaxBytes := maxBytes / c.capacity
+	for i := range c.shards {
+		c.shards[i] = newLruShard(shardMaxBytes)
+	}
+
+	return &c, nil
+}
+
+// Add value to cache under key and returns true if eviction happened
+func (c *Cache) Add(key uint64, value []byte) bool {
+	shard := c.getShard(key)
+	return shard.add(key, value)
+}
+
+func (c *Cache) Get(key uint64) ([]byte, bool) {
+	shard := c.getShard(key)
+	return shard.get(key)
+}
+
+func (c *Cache) getShard(key uint64) *lruShard {
+	bs := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bs, key)
+	hash := xxhash.Sum64(bs)
+	return c.shards[hash%c.capacity]
+}
---
+++ internal/lru/lru.go
@@ -0,0 +1,96 @@
+package lru
+
+import (
+	"container/list"
+	"sync"
+)
+
+type lruShard struct {
+	mu sync.RWMutex
+	totalBytes uint64
+	maxBytes   uint64
+	evictList *list.List
+	elems     map[uint64]*list.Element
+}
+
+func newLruShard(maxBytes uint64) *lruShard {
+	return &lruShard{
+		maxBytes: maxBytes,
+		evictList: list.New(),
+		elems: make(map[uint64]*list.Element),
+	}
+}
+
+type entry struct {
+	key   uint64
+	value []byte
+}
+
+func (ls *lruShard) get(key uint64) ([]byte, bool) {
+	ls.mu.RLock()
+	defer ls.mu.RUnlock()
+
+	if elem, ok := ls.elems[key]; ok {
+		ls.evictList.MoveToFront(elem)
+		return elem.Value.(*entry).value, true
+	} else {
+		return nil, false
+	}
+}
+
+// Add value to lru map under key and returns true if eviction happened
+func (ls *lruShard) add(key uint64, value []byte) bool {
+	ls.mu.Lock()
+	defer ls.mu.Unlock()
+
+	var evicted bool
+
+	// Check for existing item
+	if elem, ok := ls.elems[key]; ok {
+		ls.evictList.MoveToFront(elem)
+		elem.Value.(*entry).value = value
+		evicted = false
+	} else {
+		// add new item
+		elem = ls.evictList.PushFront(&entry{
+			key:   key,
+			value: value,
+		})
+
+		ls.totalBytes += uint64(len(value))
+		ls.elems[key] = elem
+		evicted = ls.totalBytes > ls.maxBytes
+
+		// Verify size not exceeded
+		if evicted {
+			ls.removeOldestUnderLock()
+		}
+	}
+
+	return evicted
+}
+
+func (ls *lruShard) purge() {
+	ls.mu.Lock()
+	defer ls.mu.Unlock()
+
+	for k := range ls.elems {
+		delete(ls.elems, k)
+	}
+
+	ls.evictList.Init()
+}
+
+func (ls *lruShard) removeOldestUnderLock() {
+	elem := ls.evictList.Back()
+	if elem != nil {
+		ls.removeElementUnderLock(elem)
+	}
+}
+
+func (ls *lruShard) removeElementUnderLock(elem *list.Element) {
+	ls.evictList.Remove(elem)
+	kv := elem.Value.(*entry)
+	delete(ls.elems, kv.key)
+	ls.totalBytes -= uint64(len(kv.value))
+}
